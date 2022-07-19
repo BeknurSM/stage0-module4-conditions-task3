66337cea9000fd017c902df62315a6c4c7158ead
BekoSm <b.smagulov.1999@gmail.com>
2022-07-19 16:03:22 +0600

    Push#3

--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
-        if (65 <= symbol <= 90 && 97 <= symbol <=122){
+        if (65 <= symbol && symbol <= 90 && 97 <= symbol && symbol <=122){
             System.out.println("English");
         }
         else{
