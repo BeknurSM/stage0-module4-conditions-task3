d2dacf316e7df15c00739ce77fa28904d0e0156f
BekoSm <b.smagulov.1999@gmail.com>
2022-07-19 15:55:22 +0600

    Push#2

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -3,10 +3,12 @@ package school.mjc.stage0.conditions.task3;
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
         if (65 <= character <= 90 && 97 <= character <=122){
-            if (character == 'a' || character =='e' || character =='i' || character =='o' || character =='u')) {
+            if (character == 'a' || character =='e' || character =='i' || character =='o' || character =='u')
+            {
                 System.out.println("Vowel");
             }
-            else{
+            else
+            {
                 System.out.println("Consonant");
             }
         }
